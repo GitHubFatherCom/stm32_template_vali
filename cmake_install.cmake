c512f5e1035329e75e654433927cc062e3b7e8b1
Vali <vasiliy26512651@mail.ru>
2022-12-08 19:11:58 +0200

    Snake initial commit

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/lab101/Documents/stm32_template_vali
+# Install script for directory: /home/kn/stm32_experiments
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/lab101/Documents/stm32_template_vali/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/kn/stm32_experiments/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
